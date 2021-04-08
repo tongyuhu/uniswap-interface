16cbba8747af8e5e30bdd4c8098707d8cf7b2e35
tongyuhu <1449807759@qq.com>
2021-04-08 16:38:05 +0800

    change logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,8 +7,8 @@ import { useTranslation } from 'react-i18next'
 
 import styled from 'styled-components'
 
-import Logo from '../../assets/svg/logo.svg'
-import LogoDark from '../../assets/svg/logo_white.svg'
+import Logo from '../../assets/images/logo.png'
+import LogoDark from '../../assets/images/logo.png'
 import { useActiveWeb3React } from '../../hooks'
 import { useDarkModeManager } from '../../state/user/hooks'
 import { useETHBalances, useAggregateUniBalance } from '../../state/wallet/hooks'
